515ad0aa4be80e14684805db055e90ebba916d4e
Jose Szychowski <joseszychowski@gmail.com>
2022-07-06 21:11:25 -0300

    Add computerPlay and playerSelection functions. Both functions works okay and return the values

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("hello world!");
+
+function computerPlay(){ //return either "Rock", "Paper", "Scissors"
+
+    let computer_choise = ["Rock", "Paper", "Scissors"];
+    computer_choise = computer_choise[Math.floor(Math.random()*computer_choise.length)]; //save one of the elements
+
+    return computer_choise;
+}
+
+function playerSelection(){
+    
+    while(true){
+
+        player_selection = prompt("Select option: \n1. Rock \n2. Paper \n3. Scissors");
+        switch (player_selection){
+            case "1": return "Rock";
+            case "2": return "Paper";
+            case "3": return "Scissors";
+            default:
+                alert("Wrong selection. Choose again.")
+        }
+    } 
+
+}
+
+console.log(playerSelection());
